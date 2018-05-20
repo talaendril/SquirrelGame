38eb7df1907a73655aea095bed67987e4a58f825
talaendril <nico-winkler@gmx.de>
2018-05-20 02:45:48 +0200

    added Images to UI

--- src/core/BoardConfig.java
+++ src/core/BoardConfig.java
@@ -4,15 +4,15 @@ import location.XY;
 
 public class BoardConfig {
 	
-	private final XY size = new XY(25, 25);
+	private final XY size = new XY(30, 30);
 	
-	private final int numberOfGoodPlants = 7;
-	private final int numberOfBadPlants = 3;
-	private final int numberOfBadBeasts = 4;
-	private final int numberOfGoodBeasts = 10;
+	private final int numberOfGoodPlants = 20;
+	private final int numberOfBadPlants = 15;
+	private final int numberOfBadBeasts = 9;
+	private final int numberOfGoodBeasts = 25;
 	private final int numberOfHandOperatedMasterSquirrels = 0;	//we are adding MasterSquirrels in the constructor of a game so this is probbaly useless
 	private final int numberOfMasterSquirrels = 0 + numberOfHandOperatedMasterSquirrels;
-	private final int numberOfRandomWalls = 11;
+	private final int numberOfRandomWalls = 40;
 	private final int numberOfWalls = 2 * size.x + 2 * size.y - 4 + numberOfRandomWalls;
 	private final int totalEntities = 
 			numberOfGoodPlants + numberOfBadPlants +
